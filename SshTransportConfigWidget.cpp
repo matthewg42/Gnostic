87e9dfe2c4fd22db682ccc0c0adcbb26a4c7cc41
Matthew Gates <matthewg42@gmail.com>
2011-03-09 12:58:10 +0000

    Refine/fix config dialog

--- SshTransportConfigWidget.cpp
+++ SshTransportConfigWidget.cpp
@@ -2,6 +2,8 @@
 #include "AbstractSshTransport.hpp"
 #include "ui_SshTransportConfigWidget.h"
 
+#include <QString>
+
 SshTransportConfigWidget::SshTransportConfigWidget(Transport* t, QWidget *parent) :
 		TransportConfigWidget(t, parent),
 		ui(new Ui::SshTransportConfigWidget)
@@ -11,6 +13,12 @@ SshTransportConfigWidget::SshTransportConfigWidget(Transport* t, QWidget *parent
 	connect(ui->descriptionEdit, SIGNAL(textEdited(QString)), sshTransport, SLOT(setDescription(QString)));
 	connect(ui->userEdit, SIGNAL(textEdited(QString)), sshTransport, SLOT(setUser(QString)));
 	connect(ui->hostEdit, SIGNAL(textEdited(QString)), sshTransport, SLOT(setHost(QString)));
+
+
+	QString subType = transport->getType();
+	subType.remove("Transport");
+
+	ui->textBrowser->append(QString("This transport uses %1.").arg(subType));
 }
 
 SshTransportConfigWidget::~SshTransportConfigWidget()
